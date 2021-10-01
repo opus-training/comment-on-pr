a8bc9d1d39fee21658356640f26946201f385630
Byrne Hollander <byrnehollander@users.noreply.github.com>
2021-10-01 15:41:35 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,22 +6,11 @@ require "octokit"
 json = File.read(ENV.fetch("GITHUB_EVENT_PATH"))
 event = JSON.parse(json)
 
-if !ENV["GITHUB_TOKEN"]
-  puts "Missing GITHUB_TOKEN"
-  exit(1)
-end
 
 github = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
 
-if ARGV[0].empty?
-  puts "Missing message argument."
-  exit(1)
-end
-
 message = ARGV[0]
-check_duplicate_msg = ARGV[1]
 delete_prev_regex_msg = ARGV[2]
-duplicate_msg_pattern = ARGV[3]
 
 repo = event["repository"]["full_name"]
 
@@ -40,27 +29,11 @@ else
   pr_number = pr["number"]
 end
 
-if check_duplicate_msg == "true"
-  coms = github.issue_comments(repo, pr_number)
+coms = github.issue_comments(repo, pr_number)
 
-  duplicate = if duplicate_msg_pattern
-    coms.find { |c| (c["body"] =~ Regexp.new(duplicate_msg_pattern)) }
-  else
-    coms.find { |c| c["body"] == message }
-  end
-
-
-  if duplicate
-    puts "The PR already contains this message"
-    exit(0)
-  end
-end
-
- if delete_prev_regex_msg != nil
-  coms.each do |n|
-    if n["body"].match(/#{delete_prev_regex_msg}/)
-	github.delete_comment(repo, n["id"], opt = {})
-    end
+coms.each do |n|
+  if n["body"].match(/#{delete_prev_regex_msg}/)
+    github.delete_comment(repo, n["id"], opt = {})
   end
 end
 
